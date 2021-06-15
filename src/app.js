3e8ea7576bbb1e16757ed17e647e417217f741c3
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-06-15 23:52:05 +1000

    Added JS for main elements
    Added JS for main elements, linked API, updated some styling in CSS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#todays-temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
+let apiKey = "82f33736fe5d08022fb7076137f7ac18";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
