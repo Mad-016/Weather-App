14da410983d9ff4bdcc0c67f1232912a56d8d2e9
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-06-16 20:27:51 +1000

    Add country, min and max as JS

--- src/app.js
+++ src/app.js
@@ -22,22 +22,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#todays-temp");
   let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
+  let minElement = document.querySelector("#min-temp");
+  let maxElement = document.querySelector("#max-temp");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  minElement.innerHTML = Math.round(response.data.main.temp_min);
+  maxElement.innerHTML = Math.round(response.data.main.temp_max);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  windElement.innerHTML = Math.round(3.6 * response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "82f33736fe5d08022fb7076137f7ac18";
-let city = "Melbourne";
+let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
