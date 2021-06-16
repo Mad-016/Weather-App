a4f5dd23a5eb577c9936b2ec1f96bf5ae29f6680
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-06-16 19:32:00 +1000

    Update Humidity and Wind

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#todays-temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "82f33736fe5d08022fb7076137f7ac18";
