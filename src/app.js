740c9146887c22e2d388fdc95fbec929db0665a5
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-06-16 19:47:17 +1000

    removed console log

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#todays-temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
